344d41946a362d9ccefb3b18cd45c23054688bf6
nubiaferr <n.ferr@hotmail.com>
2021-08-31 10:47:06 -0300

    menu 2 - em produção

--- Projeto/src/app/app.module.ts
+++ Projeto/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
 import { LogarComponent } from './logar/logar.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { Menu2Component } from './menu2/menu2.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { CadastrarComponent } from './cadastrar/cadastrar.component';
     MenuComponent,
     RodapeComponent,
     LogarComponent,
-    CadastrarComponent
+    CadastrarComponent,
+    Menu2Component
   ],
   imports: [
     BrowserModule,
---
+++ Projeto/src/app/menu2/menu2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu2',
+  templateUrl: './menu2.component.html',
+  styleUrls: ['./menu2.component.css']
+})
+export class Menu2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
